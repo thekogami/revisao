0de168a7d88a94e8675284b81de6829f1614fec8
felipemourao <felipemourao@univille.br>
2024-08-13 19:44:35 -0300

    update

--- src/tela/minhatela.java
+++ src/tela/minhatela.java
@@ -1,10 +1,25 @@
 package tela;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class minhatela extends JFrame {
     public minhatela() {
         super("Minha Tela");
         setSize(300, 300);
+
+        var meuTexto = new JLabel("Aula de Swing pratico");
+        var Uni = new JLabel("Univille");
+        meuTexto.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 20));
+        Uni.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 20));
+
+        var newBotao = new JButton("Clique aqui");
+
+
+
+        getContentPane().add("North",meuTexto);
+        getContentPane().add("Center", Uni);
+        getContentPane().add("South", newBotao);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
